2e428fbf9b08cf1ec653fd76704a118b66785c8e
jyuch <jyuch@users.noreply.github.com>
2018-07-23 22:36:30 +0900

    Update plugin description

--- src/main/scala/Plugin.scala
+++ src/main/scala/Plugin.scala
@@ -2,9 +2,9 @@ import io.github.gitbucket.helloworld.controller.HelloWorldController
 import io.github.gitbucket.solidbase.model.Version
 
 class Plugin extends gitbucket.core.plugin.Plugin {
-  override val pluginId: String = "helloworld"
-  override val pluginName: String = "HelloWorld Plugin"
-  override val description: String = "First example of GitBucket plug-in"
+  override val pluginId: String = "backup"
+  override val pluginName: String = "Backup Plugin"
+  override val description: String = "Provides data backup for GitBucket"
   override val versions: List[Version] = List(new Version("1.0.0"))
 
   override val controllers = Seq(
