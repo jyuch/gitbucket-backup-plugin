e589add5dffb1c06fa8f11c11e31e712ca1024d8
jyuch <jyuch@users.noreply.github.com>
2018-08-05 20:28:44 +0900

    Fix typo

--- src/main/scala/Plugin.scala
+++ src/main/scala/Plugin.scala
@@ -9,7 +9,7 @@ import org.slf4j.LoggerFactory
 class Plugin extends gitbucket.core.plugin.Plugin with ActorService {
   override val pluginId: String = "backup"
   override val pluginName: String = "Backup Plugin"
-  override val description: String = "Provides all in one backup features for GitBucket"
+  override val description: String = "Provide all in one backup features for GitBucket"
   override val versions: List[Version] = List(new Version("1.0.0"))
 
   private val logger = LoggerFactory.getLogger(classOf[Plugin])
